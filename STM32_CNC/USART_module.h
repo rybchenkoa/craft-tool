676ec7efa234c55292dc3d85d62de5a3abcc1b1f
Андрей Рыбченко <rybchenkoa@users.noreply.github.com>
2014-07-25 20:30:09 +0000

    Добавлена проверка переполнения при отсылке данных.

--- STM32_CNC/USART_module.h
+++ STM32_CNC/USART_module.h
@@ -75,6 +75,9 @@ class Usart
 //----------------------------------------------------------
 	void send_packet(char *data, int size)
 	{
+		if(transmitBuffer.Count() + size + 5 > transmitBuffer.Size())
+			return;
+			
 		transmitBuffer.Push(OP_CODE);
 		transmitBuffer.Push(OP_RUN);
 		for(char *endp = data+size; data != endp; data++)
