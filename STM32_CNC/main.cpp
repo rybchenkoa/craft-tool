547831d689e5b37a97d2315565f6327474eb18b3
Андрей Рыбченко <rybchenkoa@users.noreply.github.com>
2017-02-04 21:27:56 +0000

    Немного логов в главном цикле.

--- STM32_CNC/main.cpp
+++ STM32_CNC/main.cpp
@@ -45,24 +45,42 @@ int main()
 	int timeToSend = timer.get_ms(500);
 	int stepTime; //время обработки одного шага
 	mover.canLog = true;
+	int numShow = 0;
 	while(1)
 	{
 		stepTime = timer.get();
 		mover.update();
 		stepTime = timer.get() - stepTime;
-
+		
+		if (mover.canLog)
+		{
+			//log_console("dest %d %d %d, sc %d\n", mover.to[0], mover.to[1], mover.to[2]);
+		//	log_console("stepT %d, st %d, tmr %d\n", stepTime, mover.linearData.state, timer.get());
+		//	log_console("queue %d, ulen %d, trcnt %d\n", receiver.queue.Count(), receiver.tracks.Front().segments, receiver.tracks.Count());
+			//log_console("mot %d, %d, %d\n", motor[0]._isHardware, motor[1]._isHardware, motor[2]._isHardware);
+			//log_console("err %d, %d, %d\n", mover.linearData.err[0], mover.linearData.err[1], mover.linearData.err[2]);
+		//	log_console("ervr: %d, %d, %d\n", mover.linearData.error[0], mover.linearData.error[1], mover.linearData.error[2]);
+		//	log_console("coord: %d, %d, %d\n", mover.coord[0], mover.coord[1], mover.coord[2]);
+	//		log_console("coord %d, ref %d\n", mover.coord[0], virtualAxe._position);
+	//		log_console("step %d, ref %d\n", motor[0]._period, virtualAxe._period);
+			//log_console("Astep %d, %d, %d\n", get_steps(0), get_steps(1), get_steps(2));
+			//log_console("adc %d\n", adc.value());
+	//		int coord[MAX_AXES];
+	//		coord[0] = virtualAxe._position;
+	//		coord[1] = mover.coord[0];
+	//		coord[2] = 0;
+			send_packet_service_coords(mover.coord);
+		}
+		
 		if(!timer.check(timeToSend))
 		{
 			led.flip();
-			timeToSend = timer.get_ms(100);
-			//log_console("dest %d %d %d, sc %d\n", mover.to[0], mover.to[1], mover.to[2]);
-			//log_console("stepT %d, st %d\n", stepTime, mover.linearData.state);
-			//log_console("mot %d, %d, %d\n", motor[0]._isHardware, motor[1]._isHardware, motor[2]._isHardware);
-			log_console("err %d, %d, %d\n", mover.linearData.err[0], mover.linearData.err[1], mover.linearData.err[2]);
-			log_console("errX: %d\n", mover.linearData.err[0]/(iabs(mover.linearData.size[0]) + 1));
-			//log_console("Astep %d, %d, %d\n", get_steps(0), get_steps(1), get_steps(2));
-			//log_console("adc %d\n", adc.value());
-			send_packet_service_coords(mover.coord);
+			timeToSend = timer.get_ms(5);
+			numShow +=2; 
+		}
+		if (numShow > 0)
+		{
+			--numShow;
 			mover.canLog = true;
 		}
 		else
