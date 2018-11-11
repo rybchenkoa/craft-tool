e6d67807b932d74d0b72c6f9385ff07d7b0a47d4
Андрей Рыбченко <rybchenkoa@users.noreply.github.com>
2018-11-11 15:49:48 +0300

    Поправлена работа с GPIO.

--- STM32_CNC/gpio_use.h
+++ STM32_CNC/gpio_use.h
@@ -1,150 +1,202 @@
-//#define STM32F10X_LD_VL
-#include "stm32f10x.h"
+#include "stm32f407xx.h"
 #include "common.h"
+
+/*
+импульсы ШИМ можно считать вторым таймером (1 столбец)
+остальные столбцы - это таймеры, от которых ловим импульсы
+RM00090 567, 632, 674
+1	5	2	3	4		//этот для шим, его не трогаем
+8	1	2	4	5
+
+2	1	8	3	4
+3	1	2	5	4
+4	1	2	3	8		// 4<-2
+5	2	3	4	8		// 5<-3
+
+9	2	3	10	11		// 9<-10
+12	4	5	13	14		// 12<-13
+и того 4 таймера
+
+еще можно считать импульсы таймера счетчиком, который находится в канале DMA
+при этом будут делаться паразитные посылки по шине данных
+
+или сделать под каждый канал пдп массив
+это лишняя память, т.к. на 8бит счетчик надо 1кБ памяти, работает только с APB шиной
+//RM00090  308, 652
+1,0	5			// 5  DMA1 stream0, TIM5
+1,1	2	6		// 6
+1,2	3	7		// 3
+1,4	7			// 7
+1,6	4	5		// 4
+1,7	2			// 2
+
+второй поток может управлять через BSRR
+2,1	8			// 8
+2,2	u1_rx
+2,5	1	u1_rx
+2,7	u1_tx
+
+tim4 - выходы как замена bsrr
+6,7 - только через dma
+9,12 в любом случае через каскадирование таймеров - 2 канала
+
+*/
+
 
 //=====================================================================
 //разводка выводов под двигатели
-//номера сгруппированы по моторам
-//[a0,a1], [a2,a3], [a6,a7], [b7,b9], [b6,b8]
-//STEP [a0, a2, a6, b7, b6]
-//DIR [a1, a3, a7, b9, b8]
-//выводы ШИМ [b14, b15, a8, a11]
+//STEP [d12, d13, d14, d15, g4]
+//DIR [d10, d11, g2, g3, g6]
+//выводы ШИМ []
 
 #define MAX_HARD_AXES 5
 #define MAX_PWM  4
 
-#define MAX_PERIOD 65535 //максимальное время между шагами для аппаратного таймера
-#define MAX_STEP 65535 //16-битный счетчик шагов
+#define MAX_PERIOD 65535 //16-бит максимальное время между шагами для аппаратного таймера
+#define MAX_STEP 256 //8-битный счетчик шагов, 0 пропускается, вместо него 0x1 00
 
-int           STEP_PINS[]   = {0,     2,     6,     7,     6};
-GPIO_TypeDef* STEP_PORTS[]  = {GPIOA, GPIOA, GPIOA, GPIOB, GPIOB};
-int           DIR_PINS[]    = {1,     3,     7,     9,     8};
-GPIO_TypeDef* DIR_PORTS[]   = {GPIOA, GPIOA, GPIOA, GPIOB, GPIOB};
-TIM_TypeDef*  STEP_TIMERS[] = {TIM2,  TIM15, TIM3,  TIM17, TIM16};
-//int STEP_CHANNELS[] = [1, 1, 1, 1, 1] у всех step таймеров работает первый канал сравнения
-bool          CHANNEL_NEG[] = {false, false, false, true, true};
+int           DIR_PINS[]    = {10,    11,    2,     3,     6};
+GPIO_TypeDef* DIR_PORTS[]   = {GPIOD, GPIOD, GPIOG, GPIOG, GPIOG};
+TIM_TypeDef*  STEP_TIMERS[] = {TIM2,  TIM3, TIM6,  TIM7, TIM8};
+DMA_Stream_TypeDef* DMA_STREAMS[] = {DMA1_Stream7, DMA1_Stream2, DMA1_Stream1, DMA1_Stream4, DMA2_Stream1};
+int DMA_TRIGGERS[] = {DMA_CHANNEL_3, DMA_CHANNEL_5, DMA_CHANNEL_7, DMA_CHANNEL_1, DMA_CHANNEL_7};
 
-//канал DMA нужен только для того, чтобы использоваться в качестве счетчика импульсов таймера
-//RM0041 стр. 149 , каналы 2, 5, 3, 7, 6, событие TIM_UP
-DMA_Channel_TypeDef* DMA_CHANNELS[] = {DMA1_Channel2, DMA1_Channel5, DMA1_Channel3, DMA1_Channel7, DMA1_Channel6};
-char dummy;
-
-//входы b2, b10, b11, b12, a12, a15, b3, b4, b5
-GPIO_TypeDef* IN_PORTS[] = {GPIOB, GPIOB, GPIOB, GPIOB, GPIOA, GPIOA, GPIOB, GPIOB, GPIOB};
-int           IN_PINS[]  = {2,        10,    11,    12,    12,    15,     3,     4,     5};
+//входы a15, c10, c11, c12, d0, d1, d2, d3, d4
+GPIO_TypeDef* IN_PORTS[] = {GPIOA, GPIOC, GPIOC, GPIOC, GPIOD, GPIOD, GPIOD, GPIOD, GPIOD};
+int           IN_PINS[]  = {15,        10,    11,    12,    0,    1,     2,     3,     4};
 int polarity = 0; //надо ли инвертировать вход (битовый массив)
-//--------------------------------------------------
-inline void gpio_port_crl(GPIO_TypeDef *port, unsigned int mask, unsigned int val)
-{
-	int reg = port->CRL;
-	reg &= ~(mask * 0xF); //очищаем
-	reg |=  mask * val;
-	port->CRL = reg;
-}
 
-//--------------------------------------------------
-inline void gpio_port_crh(GPIO_TypeDef *port, unsigned int mask, unsigned int val)
-{
-	int reg = port->CRH;
-	reg &= ~(mask * 0xF); //очищаем
-	reg |=  mask * val;
-	port->CRH = reg;
-}
-
-#define MANUAL_OUT 1
-#define AF_OUT     9
-#define PULL_UP_IN 8
-//--------------------------------------------------
-void configure_gpio()
-{
-	//сначала STEP и DIR управляются вручную, задаем 1 - output 10 MHz
-	//DIR управляется вручную, для него задаем 1 - output 10 MHz
-	//STEP управляется таймерами, для него надо 9 - af_output 10 MHz
-	
-	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
-	RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
-	
-	//DIR A: 1, 3, 7
-	//                     76543210
-	gpio_port_crl(GPIOA, 0x10001010, MANUAL_OUT);
-	//DIR B: 9, 8
-	//                     54321098
-	gpio_port_crh(GPIOB, 0x00000011, MANUAL_OUT);
-	
-	//STEP A: 0, 2, 6,
-	//                     76543210
-	gpio_port_crl(GPIOA, 0x01000101, AF_OUT);
-	//STEP B: 7, 6
-	//                     76543210
-	gpio_port_crl(GPIOB, 0x11000000, AF_OUT);
-	
-	//PWM A: 8, 11
-	//                     54321098
-	gpio_port_crh(GPIOA, 0x00001001, AF_OUT);
-	
-	//PWM B: 14, 15
-	//                     54321098
-	gpio_port_crh(GPIOB, 0x11000000, AF_OUT);
-	
-	//входы a12, a15
-	//                     54321098
-	gpio_port_crh(GPIOA, 0x10010000, PULL_UP_IN);
-	GPIOA->BSRR = (1<<12)|(1<<15);
-	
-	//входы b2, b10, b11, b12, b3, b4, b5
-	//                     76543210
-	gpio_port_crl(GPIOB, 0x00111100, PULL_UP_IN);
-	//                     54321098
-	gpio_port_crh(GPIOB, 0x00011100, PULL_UP_IN);
-	GPIOB->BSRR = (1<<2)|(1<<3)|(1<<4)|(1<<5)|(1<<10)|(1<<11)|(1<<12);
-}
+char OC_ARRAY[MAX_STEP];
+int BSRR_ARRAY[MAX_STEP];
 
 
 //--------------------------------------------------
 // включает тактирование портов и таймеров
-void connect_timers()
+void connect_timers_clock()
 {
-	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN
-								| RCC_APB2ENR_IOPAEN
-								| RCC_APB2ENR_IOPBEN
-								| RCC_APB2ENR_TIM1EN
-								| RCC_APB2ENR_TIM15EN
-								| RCC_APB2ENR_TIM16EN
-								| RCC_APB2ENR_TIM17EN;
+  	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN
+                  | RCC_AHB1ENR_GPIOCEN
+                  | RCC_AHB1ENR_GPIODEN
+                  | RCC_AHB1ENR_GPIOGEN
+                  | RCC_AHB1ENR_DMA1EN
+                  | RCC_AHB1ENR_DMA2EN;
+
+	RCC->APB2ENR |= RCC_APB2ENR_TIM8EN;
 								
 	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN
-								| RCC_APB1ENR_TIM3EN;
+				  | RCC_APB1ENR_TIM3EN
+                  | RCC_APB1ENR_TIM4EN
+                  | RCC_APB1ENR_TIM6EN
+                  | RCC_APB1ENR_TIM7EN;
 }
 
 //--------------------------------------------------
-void config_step_timer(TIM_TypeDef *tim, DMA_Channel_TypeDef* dma, bool isNeg)
+void configure_gpio()
 {
-	tim->CR1 &= ~TIM_CR1_CEN; //останавливаем счётчик
-	tim->CCMR1  = TIM_CCMR1_OC1M_0 | TIM_CCMR1_OC1M_1; //toggle режим
-	tim->BDTR |= TIM_BDTR_MOE; //убираем блокировку выходов, описание в RM0041, стр. 235, 264
+    LL_GPIO_InitTypeDef gpio;
+	gpio.Mode = LL_GPIO_MODE_INPUT;
+	gpio.Pull = LL_GPIO_PULL_UP;
+    for (int i = 0; i < 9; ++i)
+    {
+	  gpio.Pin = 1<<IN_PINS[i];
+	  LL_GPIO_Init(IN_PORTS[i], &gpio);
+    }
 
-	tim->ARR = 65000; //(минимальная частота = 2)на каждом апдейте меняем полярность выхода
-	tim->CNT = 0;
-	tim->CCR1 = 0; //выход переключается, когда значение совпадает, пусть это происходит в конце
-	tim->CR1 |= TIM_CR1_ARPE; //сравнение CNT == ARR, поэтому чтобы не пролететь мимо во время обновления
+    //DIR управляется вручную
+	gpio.Mode = LL_GPIO_MODE_OUTPUT;
+	gpio.Speed = LL_GPIO_SPEED_FREQ_LOW;
+	gpio.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+    for (int i = 0; i < MAX_HARD_AXES; ++i)
+    {
+	  gpio.Pin = 1<<DIR_PINS[i];
+	  LL_GPIO_Init(DIR_PORTS[i], &gpio);
+    }
 	
-	//включаем выход
-	if (!isNeg)
-		tim->CCER |= TIM_CCER_CC1E | TIM_CCER_CC1P; //TIM_CCER_CC1P - обратная полярность
-	else
-		tim->CCER |= TIM_CCER_CC1NE; //TIM_CCER_CC1NP
+    //'этот STEP через BSRR
+	gpio.Pin = LL_GPIO_PIN_4;
+	LL_GPIO_Init(GPIOG, &gpio);
 
-	tim->DIER |= TIM_DIER_UDE; //запуск DMA при совпадении с ARR
-
-	dma->CCR |= DMA_CCR1_CIRC;
-	dma->CNDTR = MAX_STEP;
-	dma->CMAR = (int)&dummy;
-	dma->CPAR = (int)&TIM1->CNT;	
-	dma->CCR |= DMA_CCR1_EN;
+    // эти STEP управляется таймерами
+    gpio.Pin = GPIO_PIN_9;
+    gpio.Mode = GPIO_MODE_AF_PP;
+    gpio.Alternate = GPIO_AF2_TIM4;
+    LL_GPIO_Init(GPIOD, &gpio);
 }
 
 //--------------------------------------------------
-//задаёт режим сравнения для таймеров
+// таймер, у которого каналы сравнения используются для управления ножками
+// хорош тем, что висит на шине, доступной для обращения через DMA
+void config_output_timer(TIM_TypeDef *tim, char *arr)
+{
+  for (int i = 0; i < MAX_STEP; i+=2)
+  {
+	arr[i] = -1;
+	arr[i+1] = 0;
+  }
+
+  TIM_TypeDef *timOut = TIM14;
+  timOut->CNT = 1;
+  timOut->ARR = 2;
+  timOut->CCMR1 |= LL_TIM_OCMODE_PWM1*(1 + (1<<8)); // постоянное сравнение на <=
+  timOut->CCMR2 |= LL_TIM_OCMODE_PWM1*(1 + (1<<8));
+  timOut->BDTR |= TIM_BDTR_MOE; //убираем блокировку выходов
+  timOut->CCER |= TIM_CCER_CC1E //включаем все 4 канала сравнения
+                | TIM_CCER_CC2E
+                | TIM_CCER_CC3E
+                | TIM_CCER_CC4E;
+}
+
+//--------------------------------------------------
+//таймер, который просто генерирует запросы к DMA с нужной частотой
+void config_step_timer(TIM_TypeDef *tim)
+{
+  tim->CNT = 0;
+  tim->ARR = 0;
+  //tim->CR1 |= TIM_CR1_ARPE; //сравнение CNT == ARR, поэтому чтобы не пролететь мимо во время обновления
+  tim->DIER |= TIM_DIER_UDE; //разрешаем DMA запрос по событию переполнения (совпадение с ARR)
+}
+
+//--------------------------------------------------
+// DMA, который дергает ножкой через BSRR
+// на каждую ножку нужен отдельный массив
+void dma_bsrr_stream(DMA_Stream_TypeDef *stream, int channel,
+                    GPIO_TypeDef *port, int* arr, int bitSet, int bitReset)
+{
+  for (int i = 0; i < MAX_STEP; i+=2)
+  {
+	arr[i] = bitSet;
+	arr[i+1] = bitReset;
+  }
+
+  stream->CR |= DMA_MEMORY_TO_PERIPH
+    | DMA_SxCR_CIRC
+    | DMA_MDATAALIGN_WORD | DMA_PDATAALIGN_WORD
+    | DMA_SxCR_MINC //увеличивать указатель на память
+    | channel; //тактирование брать с этого триггера
+  stream->NDTR = 255;
+  stream->M0AR = (int)arr;
+  stream->PAR = (int)&GPIOF->BSRR;
+  stream->CR |= DMA_SxCR_EN;
+}
+
+//--------------------------------------------------
+// DMA, который переключает ножку через канал сравнения таймера
+void dma_oc_stream(int *outReg, DMA_Stream_TypeDef *stream, int channel, char *arr)
+{
+  DMA_Stream_TypeDef *stream = DMA1_Stream1;
+  stream->CR |= DMA_MEMORY_TO_PERIPH
+    | DMA_SxCR_CIRC
+    | DMA_MDATAALIGN_BYTE | DMA_PDATAALIGN_WORD
+    | DMA_SxCR_MINC //увеличивать указатель на память
+    | channel; //тактирование брать с этого триггера
+  stream->NDTR = 256;
+  stream->M0AR = (int)arr;
+  stream->PAR = (int)&outReg;	
+  stream->CR |= DMA_SxCR_EN;
+}
+
+//--------------------------------------------------
+//один таймер для каналов ШИМ
 void config_pwm_timer()
 {
 	TIM1->CR1 &= ~TIM_CR1_CEN;
@@ -166,23 +218,24 @@ void config_pwm_timer()
 	TIM1->CR1 |= TIM_CR1_CEN; //запускаем таймер
 }
 
-
 //--------------------------------------------------
-//подключение каналов шим
+//подключение шаговых таймеров
 void connect_step_channels()
 {
-	//RM0041, стр 114
-	//AFIO->MAPR |= AFIO_MAPR_TIM2_REMAP_1; //перекидываем tim2 ch3,ch4 на b10, b11
+	for (int i = 0; i < MAX_HARD_AXES; ++i) //настраиваем таймера, генерирующие шаги
+		config_step_timer(STEP_TIMERS[i]);
 
-	for (int i = 0; i < MAX_HARD_AXES; ++i)
-		config_step_timer(STEP_TIMERS[i], DMA_CHANNELS[i], CHANNEL_NEG[i]);
+    config_output_timer(TIM4, OC_ARRAY); //включаем таймер, шим каналы которого управляют ножками
+    for (int i = 0; i < 4; ++i)
+      dma_oc_stream(&TIM4->CCR1 + i, DMA_STREAMS[i], DMA_TRIGGERS[i], OC_ARRAY);
+
+    dma_bsrr_stream(DMA_STREAMS[4], DMA_TRIGGERS[4], GPIOG, BSRR_ARRAY, GPIO_BSRR_BR4, GPIO_BSRR_BS4); 
 }
-
 
 //--------------------------------------------------
 void configure_timers()
 {
-	connect_timers();
+	connect_timers_clock();
 	connect_step_channels();
 	config_pwm_timer();
 }
@@ -196,29 +249,7 @@ void __forceinline set_pin_state(GPIO_TypeDef *port, int pinnum, bool state)
 	if (state)
 		port->BSRR = 1 << pinnum;
 	else
-		port->BRR  = 1 << pinnum;
-}
-
-//--------------------------------------------------
-//включение/выключение управления ножкой порта с помощью периферии
-#define USE_AF 0x8
-void __forceinline set_port_af(GPIO_TypeDef *port, int pinnum, bool state)
-{
-	if (pinnum < 8)
-	{
-		if (state)
-			port->CRL |= USE_AF * (1<<(pinnum*4)); //на каждый пин 4 бита
-		else
-			port->CRL &= ~(USE_AF * (1<<(pinnum*4)));
-	}
-	else
-	{
-		pinnum -= 8;
-		if (state)
-			port->CRH |= USE_AF * (1<<(pinnum*4));
-		else
-			port->CRH &= ~(USE_AF * (1<<(pinnum*4)));
-	}
+		port->BSRR  = 1 << (pinnum + 16);
 }
 
 //--------------------------------------------------
@@ -240,7 +271,7 @@ bool inline get_pin(int index)
 //задает время между шагами
 void inline set_step_time(int index, int ticks)
 {
-	STEP_TIMERS[index]->ARR = ticks;
+	STEP_TIMERS[index]->ARR = ticks + 1;
 }
 
 //--------------------------------------------------
@@ -248,7 +279,6 @@ void inline set_step_time(int index, int ticks)
 void inline enable_step_timer(int index)
 {
 	STEP_TIMERS[index]->CR1 |= TIM_CR1_CEN;
-	//set_port_af(STEP_PORTS[index], STEP_PINS[index], true);
 }
 
 //--------------------------------------------------
@@ -256,14 +286,13 @@ void inline enable_step_timer(int index)
 void inline disable_step_timer(int index)
 {
 	STEP_TIMERS[index]->CR1 &= ~TIM_CR1_CEN;
-	//set_port_af(STEP_PORTS[index], STEP_PINS[index], false);
 }
 
 //--------------------------------------------------
 //возвращает число шагов, сделанных таймером
 int inline get_steps(int index)
 {
-	return -DMA_CHANNELS[index]->CNDTR;
+	return -DMA_STREAMS[index]->NDTR;
 }
 
 //--------------------------------------------------
