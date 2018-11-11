d5563c5124b4ba94d35ef8c3cb6394e6296f0d8b
Андрей Рыбченко <rybchenkoa@users.noreply.github.com>
2018-11-11 18:31:16 +0300

    Переделан таймер на TIM5.

--- STM32_CNC/sys_timer.h
+++ STM32_CNC/sys_timer.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "stm32f10x.h"
+#include "stm32f4xx.h"
 #include "common.h"
 
 class SysTimer
@@ -8,13 +8,27 @@ class SysTimer
 		int highCounter; //таймер тикает на уменьшение, поэтому здесь лежит накопленное время + макс. значение
 		void init()
 		{
-			highCounter = SysTick_LOAD_RELOAD_Msk + 1;
-			SysTick_Config(SysTick_LOAD_RELOAD_Msk);
+          /*
+          highCounter = SysTick_LOAD_RELOAD_Msk + 1;
+          SysTick_Config(SysTick_LOAD_RELOAD_Msk);
+          */
+          /*
+          CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+	      DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
+          */
+          RCC->APB1ENR |= RCC_APB1ENR_TIM5EN;
+          TIM5->PSC = 83; //APB1*2 - 84 МГц
+          TIM5->CNT = 0;
+          TIM5->ARR = -1;
+          TIM5->EGR |= TIM_EGR_UG; //обновляем значение в PSC
+          TIM5->CR1 |= TIM_CR1_CEN;
 		}
 		
 		inline int get()
-		{//DWT->CYCCNT
-			return (highCounter - SysTick->VAL); //mks*24
+		{
+            //return DWT->CYCCNT;
+            //return (highCounter - SysTick->VAL); //mks*168
+			return TIM5->CNT; //mks
 		}
 		
 		void delay_ms(int ms)
@@ -31,17 +45,17 @@ class SysTimer
 		
 		inline int get_ms(int ms)
 		{
-			return ms*1000*24 + get();
+			return ms*1000 + get();
 		}
 		
 		inline int get_mks(int mks)
 		{
-			return mks*24 + get();
+			return mks + get();
 		}
 		
 		inline int get_mks(int from, int mks)
 		{
-			return mks*24 + from;
+			return mks + from;
 		}		
 		
 		inline bool check(int endTime)
@@ -51,8 +65,9 @@ class SysTimer
 };
 
 SysTimer timer;
-
+/*
 extern "C" void SysTick_Handler(void)
 {
 	timer.highCounter += SysTick_LOAD_RELOAD_Msk + 1;
 }
+*/
