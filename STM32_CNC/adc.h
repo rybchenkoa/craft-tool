3323c3845a665f410621c3483857af1761a04441
Андрей Рыбченко <rybchenkoa@users.noreply.github.com>
2018-11-11 16:47:23 +0300

    Условно исправлен АЦП.

--- STM32_CNC/adc.h
+++ STM32_CNC/adc.h
@@ -1,4 +1,4 @@
-
+#include "stm32f407xx.h"
 class Adc
 {
 	public:
@@ -9,34 +9,37 @@ class Adc
 	
 	void init()
 	{
-		//A: 0,1,2,3,6,7,8,11
-		//               76543210
-		GPIOA->CRL &= ~0x000F0000; 						//включаем
-		//GPIOA->CRL |=  0x00000000;          //analog input
+        RCC->AHB1ENR |= RCC_AHB1ENR_GPIOFEN;
+
+        LL_GPIO_InitTypeDef gpio;
+        gpio.Pin = LL_GPIO_PIN_3;
+        gpio.Mode = LL_GPIO_MODE_ANALOG;
+        gpio.Pull = LL_GPIO_PULL_NO;
+        LL_GPIO_Init(GPIOF, &gpio);
 		
 		RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;   //подключаем тактирование adc
-		ADC1->CR2 |= ADC_CR2_ADON;            //включаем сам adc
-		//ADC1->SR & ADC_SR_STRT              //ждём, пока заведётся
+
+        ADC_TypeDef *adc = ADC3;
+		adc->CR2 |= ADC_CR2_ADON;            //включаем сам adc
+		//adc->SR & ADC_SR_STRT              //ждём, пока заведётся
 		
-		ADC1->SQR1 &= ~(ADC_SQR1_L);          //меряем один канал
-		ADC1->SQR3 |= ADC_SQR3_SQ1_0 * 4;     //первым будем мерять четвёртый канал
-
+		adc->SQR1 &= ~(ADC_SQR1_L);          //меряем один канал
+		adc->SQR3 |= ADC_SQR3_SQ1_0 * 9;     //первым будем мерять четвёртый канал
 		/*
-		ADC1->JSQR &= ~(ADC_JSQR_JL_0 | ADC_JSQR_JL_1); //используем один выделенный канал
-		ADC1->JSQR |= ADC_JSQR_JSQ1_0 * 4;    //первым будем мерять четвёртый канал
-
+		adc->JSQR &= ~(ADC_JSQR_JL_0 | ADC_JSQR_JL_1); //используем один выделенный канал
+		adc->JSQR |= ADC_JSQR_JSQ1_0 * 4;    //первым будем мерять четвёртый канал
 		*/
 		
-		ADC1->CR2 |= ADC_CR2_CONT;            //постоянно
-		ADC1->SMPR1 = 0;                      //с максимальной частотой
+		adc->CR2 |= ADC_CR2_CONT;            //постоянно
+		adc->SMPR1 = 0;                      //с максимальной частотой
 
 		/*
-		ADC1->CR1 |= ADC_CR1_SCAN;
-		ADC1->CR2 |= ADC_CR2_JSWSTART;        //шлём сигнал о начале измерений
+		adc->CR1 |= ADC_CR1_SCAN;
+		adc->CR2 |= ADC_CR2_JSWSTART;        //шлём сигнал о начале измерений
 		*/
-		ADC1->CR2 |= ADC_CR2_EXTSEL;         //старт по SWSTART
-		ADC1->CR2 |= ADC_CR2_EXTTRIG;        //
-		ADC1->CR2 |= ADC_CR2_SWSTART;        //шлём сигнал о начале измерений
+		adc->CR2 |= ADC_CR2_EXTSEL;         //старт по SWSTART
+		adc->CR2 |= ADC_CR2_EXTEN;        //
+		adc->CR2 |= ADC_CR2_SWSTART;        //шлём сигнал о начале измерений
 	}
 };
 
